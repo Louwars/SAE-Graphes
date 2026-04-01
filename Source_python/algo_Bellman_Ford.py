ce9ff35176d7998e4a287d06f54bb3e7048e2354
Noah Cheminard <noah.cheminard@etu.univ-nantes.fr>
2026-04-01 12:02:52 +0200

    Test BellmanFord

--- Source_python/algo_Bellman_Ford.py
+++ Source_python/algo_Bellman_Ford.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import random
 
 from graphe_non_oriente import GrapheValueNonOriente
 from algo_plus_court_chemin import AlgoPlusCourtChemin
@@ -72,19 +73,93 @@ if __name__ == "__main__":
     algoBF = AlgoBellmanFord(g)
     print("\n Distances sur les plus courts chemins entre les sommets :\n", algoBF.distances)
     print("\nPrédécesseurs sur les plus courts chemins entre les sommets :\n", algoBF.predecesseurs)
-    
-    
+
     ###################################################
     ### Moyen graphe : graphe de 20 sommets et 35 arêtes
-    
+
     print("\n ##### Moyen graphe #####\n")
-    
-    
-    
-    
+
+    matrice = np.array([
+        [math.inf, 1, math.inf, 1, math.inf, 5, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 8],
+        [1, math.inf, 1, 1, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, 4, math.inf, math.inf],
+        [math.inf, 1, math.inf, 1, 1, math.inf, math.inf, math.inf, math.inf, math.inf, 7, math.inf, math.inf, math.inf,
+         math.inf, 3, math.inf, math.inf, math.inf, math.inf],
+        [1, 1, 1, math.inf, 1, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 6, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, 1, 1, math.inf, 1, math.inf, 2, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, 5, math.inf],
+        [5, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf, math.inf, math.inf, math.inf, 3, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf, 2, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, 2, math.inf, 1, math.inf, 1, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 4],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf, 2, math.inf,
+         math.inf, 6, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf,
+         math.inf, 4, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, 7, math.inf, math.inf, math.inf, 2, math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 2, math.inf, 1, math.inf, 1,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, 6, math.inf, 3, math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 4, math.inf,
+         math.inf, 1, math.inf, 1, math.inf, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 6, math.inf, math.inf,
+         math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, 3, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf, math.inf],
+        [math.inf, 4, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1, math.inf],
+        [math.inf, math.inf, math.inf, math.inf, 5, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf, 1],
+        [8, math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 4, math.inf, math.inf, math.inf, math.inf,
+         math.inf, math.inf, math.inf, math.inf, math.inf, math.inf, 1, math.inf]
+    ])
+    g = GrapheValueNonOriente(matrice)
+
+    print("nb sommets : ", g.nb_sommets())
+    print("nb arêtes : ", g.nb_aretes())
+
+    # Utilisation de l'algorithme de BellmanFord
+    algoBellmanFord = AlgoBellmanFord(g)
+    print("\n Distances sur les plus courts chemins entre les sommets :\n", algoBellmanFord.distances)
+    print("\nPrédécesseurs sur les plus courts chemins entre les sommets :\n", algoBellmanFord.predecesseurs)
+
     ###################################################
     ### Grand graphe : graphe de 200 sommets et 350 arêtes
-    
+
     print("\n ##### Grand graphe #####\n")
+    n = 200  # sommets
+    m = 350  # arêtes
+
+    # Matrice remplie avec inf
+    matrice = np.full((n, n), math.inf)
+
+    edges = set()
+
+    while len(edges) < m:
+        i = random.randint(0, n - 1)
+        j = random.randint(0, n - 1)
+
+        if i != j and (i, j) not in edges and (j, i) not in edges:
+            matrice[i][j] = 1
+            matrice[j][i] = 1  # graphe non orienté
+            edges.add((i, j))
+    g = GrapheValueNonOriente(matrice)
+
+    print("nb sommets : ", g.nb_sommets())
+    print("nb arêtes : ", g.nb_aretes())
+
+    # Utilisation de l'algorithme de BellmanFord
+    algoBellmanFord = AlgoBellmanFord(g)
+    print("\n Distances sur les plus courts chemins entre les sommets :\n", algoBellmanFord.distances)
+    print("\nPrédécesseurs sur les plus courts chemins entre les sommets :\n", algoBellmanFord.predecesseurs)
+
     
         
